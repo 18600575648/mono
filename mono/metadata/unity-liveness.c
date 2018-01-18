498d5dd62caa7fe35a70bca4e07ea5a980c95636
Jonathan Chambers <joncham@gmail.com>
2018-01-18 13:53:36 -0500

    Use checked version of API

--- mono/metadata/unity-liveness.c
+++ mono/metadata/unity-liveness.c
@@ -435,10 +435,10 @@ void mono_unity_liveness_calculation_from_statics(LivenessState* liveness_state)
 			}
 			else
 			{
-				MonoError error;
+				ERROR_DECL (error);
 				MonoObject* val = NULL;
 
-				mono_field_static_get_value_checked (mono_class_vtable (domain, klass), field, &val, &error);
+				mono_field_static_get_value_checked (mono_class_vtable_checked (domain, klass, &error), field, &val, &error);
 
 				if (val && mono_error_ok (&error))
 				{
